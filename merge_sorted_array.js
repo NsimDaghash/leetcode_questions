9bbd1c70de166c6a26fe978b9bad428a57c7c8a6
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-06-12 22:51:57 +0300

    merge two sorted array
    merge two sorted array

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -1,19 +1,14 @@
 /*
 88. Merge Sorted Array
-Easy
 
-5661
-
-508
-
-Add to List
-
-Share
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+representing the number of elements in nums1 and nums2 respectively.
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
  
 
@@ -36,6 +31,18 @@ Output: [1]
 Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 */
 /*
  * @param {number[]} nums1
@@ -44,21 +51,23 @@ Note that because m = 0, there are no elements in nums1. The 0 is only there to 
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- let merge = function(nums1, m, nums2, n) {
-    let tmparr = [];
-    for( let i=0;i<m-1;i++){
-        for( let j=0;j<n-1;j++){
-            if(nums2[j]<nums1[i]){
-                tmparr.push(nums2[j])
-                console.log(tmparr);
-            }
-            if(nums2[j]>nums1[i]){
-                tmparr.push(nums1[i])
-                console.log(tmparr);
-            }
-        }
+let merge = function(nums1, m, nums2, n) {
+    m=m-1;
+    let i = m+n;
+    n=n-1;    
+    while (i >= 0) {
+      if (n > -1 && m > -1 && nums1[m] >= nums2[n]) {
+        nums1[i] = nums1[m];
+        nums1[m] = nums2[n];
+        m--;
+      }
+      else if (n > -1) {
+        nums1[i] = nums2[n--];
+      }
+      i--;
     }
-    return tmparr;
+    console.log(nums1);
 };
-let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-console.log(merge(nums1, m, nums2, n));
+
+let nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+merge(nums1, m, nums2, n);
