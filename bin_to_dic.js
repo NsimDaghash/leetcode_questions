075f0c0c17d257b4100282877d72b50ef0643c3e
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-06-25 23:28:19 +0300

    binary to decimal
    Complete the function which converts a binary number (given as a string) to a decimal number.

---
+++ bin_to_dic.js
@@ -0,0 +1,10 @@
+/*
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
+function binToDec(bin){
+    bin =  parseInt(bin, 2);
+    return bin
+  }
+
+  let bin = "1001001";
+  console.log(binToDec(bin));
