c52db3ee9bbbcd62bf21aea067791afc7e5b9673
nasimdaghash <87161661+nasimdaghash@users.noreply.github.com>
2022-09-13 00:16:21 +0300

    more questions solved from leetcode

--- to_lower_case.js
+++ to_lower_case.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 709. To Lower Case  // leetcode question 
 
@@ -27,34 +27,5 @@ let toLowerCase = function(s) {
 };
 
 let s= "Lovely";
-=======
-/*
-709. To Lower Case  // leetcode question 
 
-Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
-
-Example 1:
-
-Input: s = "Hello"
-Output: "hello"
-Example 2:
-
-Input: s = "here"
-Output: "here"
-Example 3:
-
-Input: s = "LOVELY"
-Output: "lovely"
-*/
-/*
- * @param {string} s
- * @return {string}
- */
-let toLowerCase = function(s) {
-    s=s.toLowerCase();
-    return s;
-};
-
-let s= "Lovely";
->>>>>>> c13846b7f3aace80eb018a40fb934c865aa5fa78
 console.log(toLowerCase(s));
